6826b582486295fb453005f3e118b3ffe0d13dc9
xuanx1 <tanx329@newschool.edu>
2024-10-20 00:52:51 -0400

    loaded

--- app.js
+++ app.js
@@ -7,9 +7,6 @@
 //OR verbs vs nouns - translate into call to action vs reference, ignore stop words
 
 
-import Sentiment from "sentiment";
-
-const sentiment = new Sentiment();
 const margin = { top: 30, right: 50, bottom: 30, left: 80 };
 
 
